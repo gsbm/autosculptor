75d602a557b9ba58d803df93ee318fe7623965d4
Regis <31590269+greenmagenta@users.noreply.github.com>
2024-05-20 13:24:14 +0200

    Update version

--- autosculptor.py
+++ autosculptor.py
@@ -8,7 +8,7 @@ bl_info = {
     "category": "Object",
     "description": "Generate 3D models using generative models.",
     "author": "Greenmagenta",
-    "version": (1, 0, 0),
+    "version": (1, 1, 0),
     "location": "View3D > Sidebar > Autosculptor",
     "support": "COMMUNITY",
     "doc_url": "https://github.com/greenmagenta/autosculptor",
