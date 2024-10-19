3f41585bea2f14a823c4e7d97030da580a81094f
gsbm <31590269+gsbm@users.noreply.github.com>
2024-10-19 13:38:40 +0200

    Update utils.py

--- autosculptor/utils.py
+++ autosculptor/utils.py
@@ -4,11 +4,15 @@ import subprocess
 def ensure_gradio_installed():
     try:
         import gradio_client
-        return True
+        from packaging import version
+        if version.parse(gradio_client.__version__) >= version.parse("1.4.0"):
+            return True
+        else:
+            return False
     except ImportError:
         return False
 
 def install_gradio():
     python_executable = sys.executable
     subprocess.check_call([python_executable, '-m', 'ensurepip'])
-    subprocess.check_call([python_executable, '-m', 'pip', 'install', 'gradio_client==1.2.0'])
+    subprocess.check_call([python_executable, '-m', 'pip', 'install', 'gradio_client==1.4.0'])
